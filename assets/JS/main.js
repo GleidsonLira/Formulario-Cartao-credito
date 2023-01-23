290b98fff6a95fdf43771d554a86bfe4084458b3
Gleidson Lira <lira.spfc@gmail.com>
2023-01-23 16:24:44 -0300

    Mais adições no Javascript

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -8,3 +8,23 @@ let numberCard = "";
 let month = "";
 let year = "";
 let cvc = "";
+
+//Recebendo o input button
+const confirm = document.getElementById('confirm');
+
+//variáveis que identificam se tornam verdadeiras caso não ocorram erros
+let teste1 = false
+let teste2 = false
+let teste3 = false
+
+// Busca todos os elementos onde serão inseridos os erros caso necessario
+const errorNumberCard = document.getElementById('errorNumberCard')
+const errorMonth = document.getElementById('errorMonth')
+const errorCvc = document.getElementById('errorCvc')
+
+// Busca todos os elementos inputs
+const userNameInput = document.getElementById('name')
+const numberCardInput = document.getElementById('number')
+const monthInput = document.getElementById('month')
+const yearInput = document.getElementById('year')
+const cvcInput = document.getElementById('cvc')
