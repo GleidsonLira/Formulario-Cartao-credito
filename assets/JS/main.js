3f5849b2c4cc7f271ad2aa83b638d928a42030f9
Gleidson Lira <lira.spfc@gmail.com>
2023-01-23 16:17:18 -0300

    Iniciando construção Javascript

---
+++ assets/JS/main.js
@@ -0,0 +1,10 @@
+// Constantes que pegam os meus elementos div da tela do meu html
+const visible = document.querySelector('.visible')
+const ocult = document.querySelector('.ocult')
+
+//Receber inputs da tela
+let userName = "";
+let numberCard = "";
+let month = "";
+let year = "";
+let cvc = "";
